997a01a26ebee16dcca1c77564ae8e9da159f8d4
pelumi454 <amoochristianah454@gmail.com>
2022-03-20 18:50:02 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/404/404.page";
 import Dashboard from "./pages/index/index.page";
 import MarketerPage from "./pages/index/_id/marketer.page";
 import LoginPage from "./pages/login/login.page";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   const [user, setUser] = useState(null);
